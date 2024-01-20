3b52c852ef0262b52421b16eb0397c4b2df2cf2a
0xb79orch <0xb79orch@gmail.com>
2024-01-19 23:05:14 -0600

    remove num_inference_steps

--- sd-video-api.py
+++ sd-video-api.py
@@ -177,13 +177,12 @@ async def generate_video(request_data: UploadFile | None = None, livepeer_job: A
             seed = int(params.get("seed",-1))
             fps = int(params.get("fps", 10))
             num_frames = int(params.get("num_frames", 25))
-            num_inference_steps = int(params.get("num_inference_steps"), 25)
             motion_bucket_id = int(params.get("motion_bucket_id", 127))
             noise_aug_strength = float(params.get("noise_aug_strength", 0.02))
             duration = int(params.get("duration", 2))
             generator = torch.manual_seed(seed)
             
-            frames = pipe(image, decode_chunk_size=decode_chunk_size, generator=generator, num_inference_steps=num_inference_steps, motion_bucket_id=motion_bucket_id, noise_aug_strength=noise_aug_strength, num_frames=num_frames).frames[0]
+            frames = pipe(image, decode_chunk_size=decode_chunk_size, generator=generator, motion_bucket_id=motion_bucket_id, noise_aug_strength=noise_aug_strength, num_frames=num_frames).frames[0]
             
             output_file = DATA_PATH+"/"+job["id"]+".mp4"
             print("saving output to: "+output_file)
